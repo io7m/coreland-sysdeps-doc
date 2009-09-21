97f4821921e565cedeb94b12241091f21acdd86c
m0 <m0@viper.internal.network>
2009-09-21 12:34:05 +0100

    First pile of documentation.

--- local.sh
+++ local.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
 
+fatal()
+{
+  echo "fatal: $1" 1>&2
+  exit 1
+}
+
+SYSDEPS_SOURCE="$HOME/git/coreland/sysdeps"
+
+# Create sd-arch output.
+(
+echo 'UNKNOWN'
+awk '{print $2}' < ${SYSDEPS_SOURCE}/GENERATION/arch.txt
+) > src/dd-comp-arch.txt || fatal "could not write src/dd-comp-arch.txt"
