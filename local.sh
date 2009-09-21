3022bf63ab5a9e79aff0c3fa0a9027f221784b31
m0 <m0@viper.internal.network>
2009-09-21 13:49:08 +0100

    Document first few component programs.

--- local.sh
+++ local.sh
@@ -10,6 +10,13 @@ SYSDEPS_SOURCE="$HOME/git/coreland/sysdeps"
 
 # Create sd-arch output.
 (
-echo 'UNKNOWN'
-awk '{print $2}' < ${SYSDEPS_SOURCE}/GENERATION/arch.txt
+echo 'SD_SYSINFO_ARCH_UNKNOWN'
+awk '{printf "SD_SYSINFO_ARCH_%s\n",$2}' < ${SYSDEPS_SOURCE}/GENERATION/arch.txt
 ) > src/dd-comp-arch.txt || fatal "could not write src/dd-comp-arch.txt"
+
+# Create sd-cctype output.
+(
+echo 'SD_SYSINFO_CCTYPE_UNKNOWN'
+awk '{printf "SD_SYSINFO_CCTYPE_%s\n",$2}' < ${SYSDEPS_SOURCE}/GENERATION/cctype.txt
+) > src/dd-comp-cctype.txt || fatal "could not write src/dd-comp-cctype.txt"
+
