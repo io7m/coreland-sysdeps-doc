013756bbbe66e436db53f144f43d794542735ad2
m0 <m0@viper.internal.network>
2009-09-21 16:33:42 +0100

    Further progress on documentation.

--- local.sh
+++ local.sh
@@ -8,13 +8,47 @@ fatal()
 
 SYSDEPS_SOURCE="$HOME/git/coreland/sysdeps"
 
-# Create sd-arch output.
 (
-echo 'SD_SYSINFO_ARCH_UNKNOWN'
-awk '{printf "SD_SYSINFO_ARCH_%s\n",$2}' < ${SYSDEPS_SOURCE}/GENERATION/arch.txt
-) > src/dd-comp-arch.txt || fatal "could not write src/dd-comp-arch.txt"
+IFS="
+"
+cat <<EOF
+(table arches
+  (t-row
+    (item "Value")
+    (item "Description")
+    (item "URL"))
+EOF
 
+LINE=1
+for line in `cat ${SYSDEPS_SOURCE}/GENERATION/arch.txt`
+do
+  cc_type="SD_SYSINFO_ARCH_`echo ${line} | awk '{print $2}'`"
+  desc_line=`sed -e "${LINE}q;d" < ${SYSDEPS_SOURCE}/GENERATION/arch_desc.txt`
+  desc=`echo ${desc_line} | awk -F\| '{print $2}'`
+  url=`echo ${desc_line}  | awk -F\| '{print $3}' | tr -d ' '`
+
+  if [ "${url}" = "n/a" ]
+  then
+    item="\"n/a\""
+  else
+    item="(link-ext \"${url}\")"
+  fi
+
+  cat <<EOF
+  (t-row
+    (item (item constant "${cc_type}"))
+    (item "${desc}")
+    (item ${item}))
+EOF
+  LINE=`expr ${LINE} + 1`
+done
+echo ")"
+) > src/dd-comp-arch-table.ud ||
+  fatal "could not write src/dd-comp-arch-table.ud"
+
+#
 # Create sd-cctype output.
+#
 (
 IFS="
 "
@@ -32,13 +66,20 @@ do
   cc_type="SD_SYSINFO_CCTYPE_`echo ${line} | awk '{print $2}'`"
   desc_line=`sed -e "${LINE}q;d" < ${SYSDEPS_SOURCE}/GENERATION/cctype_desc.txt`
   desc=`echo ${desc_line} | awk -F\| '{print $2}'`
-  url=`echo ${desc_line}  | awk -F\| '{print $3}'`
+  url=`echo ${desc_line}  | awk -F\| '{print $3}' | tr -d ' '`
+
+  if [ "${url}" = "n/a" ]
+  then
+    item="\"n/a\""
+  else
+    item="(link-ext \"${url}\")"
+  fi
 
   cat <<EOF
   (t-row
     (item (item constant "${cc_type}"))
     (item "${desc}")
-    (item (link-ext "${url}")))
+    (item ${item}))
 EOF
   LINE=`expr ${LINE} + 1`
 done
