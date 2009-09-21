cce22f8915c092829becea7c0a207d5e6e2ce7b6
m0 <m0@viper.internal.network>
2009-09-21 15:26:02 +0100

    Generate compiler data table.

--- local.sh
+++ local.sh
@@ -16,7 +16,32 @@ awk '{printf "SD_SYSINFO_ARCH_%s\n",$2}' < ${SYSDEPS_SOURCE}/GENERATION/arch.txt
 
 # Create sd-cctype output.
 (
-echo 'SD_SYSINFO_CCTYPE_UNKNOWN'
-awk '{printf "SD_SYSINFO_CCTYPE_%s\n",$2}' < ${SYSDEPS_SOURCE}/GENERATION/cctype.txt
-) > src/dd-comp-cctype.txt || fatal "could not write src/dd-comp-cctype.txt"
+IFS="
+"
+cat <<EOF
+(table cctypes
+  (t-row
+    (item "Value")
+    (item "Description")
+    (item "URL"))
+EOF
 
+LINE=1
+for line in `cat ${SYSDEPS_SOURCE}/GENERATION/cctype.txt`
+do
+  cc_type="SD_SYSINFO_CCTYPE_`echo ${line} | awk '{print $2}'`"
+  desc_line=`sed -e "${LINE}q;d" < ${SYSDEPS_SOURCE}/GENERATION/cctype_desc.txt`
+  desc=`echo ${desc_line} | awk -F\| '{print $2}'`
+  url=`echo ${desc_line}  | awk -F\| '{print $3}'`
+
+  cat <<EOF
+  (t-row
+    (item (item constant "${cc_type}"))
+    (item "${desc}")
+    (item (link-ext "${url}")))
+EOF
+  LINE=`expr ${LINE} + 1`
+done
+echo ")"
+) > src/dd-comp-cctype-table.ud ||
+  fatal "could not write src/dd-comp-cctype-table.ud"
